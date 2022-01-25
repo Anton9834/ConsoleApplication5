92c117e1cd08c65beaf2bb88057ff70623d1f028
anton.sergeev.4568 <anton.sergeev.4568@gmail.com>
2022-01-25 18:25:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class CarFactory {
+private:
+    CarFactory(){}
+    static int counter;
+    static void Create();
+    static void Count();
+   
+};
+void CarFactory::Create() {
+
+}
+void CarFactory::Count() {
+    CarFactory::counter = 0;
+}
+
+
+
+
+int main()
+{
+  
+}
+
